e61b717216327e3b5cd0e23ec0afa29d35c6ec91
ml-ak <a.kiviniemi@protonmail.com>
2026-08-14 13:12:31 +0300

    cleaned up the gradio_app.py codes.

--- gradio_app.py
+++ gradio_app.py
@@ -158,14 +158,5 @@ iface = gr.Interface(
 )
 
 # Launch the Gradio App
-#if __name__ == "__main__":
-#    iface.launch()
-
-# new version of launch. 
 if __name__ == "__main__":
     iface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
-
-#if __name__ == "__main__":
-#    iface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
-
-
